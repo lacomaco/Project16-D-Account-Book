7936b02b64e3cc67bfb1d0d635615ebe2f5b98a1
lacomaco <lacomaco122@gmail.com>
2021-01-09 19:18:08 +0900

    test : 거래내역생성 , 삭제 테스트 코드 작성
    - 거래내역 생성 테스트 코드를 작성하였습니다.
      - 금액,카테고리,결제수단,내용등을 임의로 쓴 결제내역을 하나
        생성합니다.
      - 거래일은 현재 테스트한 일을 기준으로 생성됩니다.
    - 거래내역 삭제 테스트 코드를 작성하였습니다.
      - 이전에 생성한 테스트용 거래내역 데이터를 삭제함으로써 테스트 검증을
        수행합니다.

--- client/cypress/integration/createTransaction.spec.js
+++ client/cypress/integration/createTransaction.spec.js
@@ -1,21 +1,42 @@
 import { visitAccountBook } from '../utils';
-
+import path from 'path';
+const serverPath = path.resolve('./../../../server');
+console.log(__dirname);
+console.log(serverPath);
 describe('createTransaction Test', () => {
+  before(() => {
+    cy.exec(`npm run db:reset`);
+    cy.exec('npm run db:seed');
+  });
   beforeEach(() => {
     visitAccountBook();
-    cy.get('svg[aria-label="button-create-modal"]').click();
   });
   it('createTransaction in modal', () => {
-    //given
-    /*
-    금액
-    카테고리
-    결제수단
-    내용
-    날짜
-    메모
-    */
-    //when
-    //then
+    cy.get('svg[aria-label="button-create-modal"]').click();
+    //금액
+    cy.get('input[placeholder="금액"]').type('555');
+    //카테고리
+    cy.contains('p', '카테고리').click();
+    cy.contains('타행이체').click();
+    //결제수단
+    cy.contains('결제수단을 선택하세요').click();
+    cy.contains('삼성카드').click();
+    // 내용
+    cy.get('input[placeholder="내용"]').type('치킨을 타행이체 하였습니다');
+    //날짜
+    const date = new Date();
+    date.setMilliseconds(0);
+    date.setSeconds(0);
+    const currentDate = date.toISOString().substring(0, date.toISOString().length - 8);
+    cy.get('input[type="datetime-local"]').type(currentDate);
+    //메모
+    cy.get('input[placeholder="메모"]').type('비고');
+    cy.contains('완료').click();
+    expect(cy.contains('비고')).to.not.equal(null);
+  });
+  it('delete Transaction', () => {
+    cy.contains('비고').click();
+    cy.contains('삭제').click();
+    cy.contains('비고').should('not.exist');
   });
 });
